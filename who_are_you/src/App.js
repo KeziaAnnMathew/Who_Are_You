c4169203287d12d102d80ebe15d8e0d29f5fe3c5
KeziaAnnMathew <74422208+KeziaAnnMathew@users.noreply.github.com>
2022-09-23 22:57:46 -0500

    Table structure

--- who_are_you/src/App.js
+++ who_are_you/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Staff Visit History</h2>
+      <div className="Logtable">
+        <table>
+          <tr>
+            <th>
+              Staff Name
+            </th>
+            <th>
+              Entry Time
+            </th>
+            <th>
+              Exit Time
+            </th>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
