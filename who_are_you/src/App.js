3f64a73ca7ea88fb2508379015812063fe2ffbc5
KeziaAnnMathew <74422208+KeziaAnnMathew@users.noreply.github.com>
2022-09-24 22:34:27 -0500

    collection

--- who_are_you/src/App.js
+++ who_are_you/src/App.js
@@ -35,20 +35,19 @@ function App() {
   }, [data]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-          Edit <code>src/App.js</code> and save to reload.
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <Badge/> */}
-      </header>
+      <h2>Staff Log</h2>
+      <div>
+        <table>
+          <tr>
+            <th>name</th>
+          </tr>
+          <tr>
+            <td>{data.name}</td>
+          </tr>
+        </table>
+      </div>
+      
+      
     </div>
   );
 }
